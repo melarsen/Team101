368584f7c3c9972b7e79e5ca5b116bcd08b67b53
mettelarsen <mette@proteria.com>
2024-03-18 19:41:14 +0100

    first commit

---
+++ 2024-Easterhunt/Hangman Game/scripts/easter-words.js
@@ -0,0 +1,18 @@
+const wordList = [
+    {
+        word: "mellomrom",
+        hint: "Noe du bruker hver dag!"
+    },
+    {
+        word: "astronomi",
+        hint: "En vitenskap vi kan lære mye mer om!"
+    },
+    {
+        word: "sjokolade",
+        hint: "Noe godt man kan spise."
+    },
+    {
+        word: "kryptovaluta",
+        hint: "Noe du kan kjøpe, men ikke kan holde i."
+    },
+];
