50d9a0725ca275544c5ae0cacfc6522d0d254a4d
mettelarsen <mette@proteria.com>
2025-04-14 22:37:27 +0200

    easterhunt 2025

---
+++ 2025-Easterhunt/scripts/easter-words.js
@@ -0,0 +1,34 @@
+const wordList = [
+    {
+        word: "mellomrom",
+        hint: "Noe du bruker hver dag!"
+    },
+    {
+        word: "astronomi",
+        hint: "En vitenskap vi kan lære mye mer om!"
+    },
+    {
+        word: "sjokolade",
+        hint: "Noe godt man kan spise."
+    },
+    {
+        word: "kryptovaluta",
+        hint: "Noe du kan kjøpe, men ikke kan holde i."
+    },
+    {
+        word: "kjøkkenmaskin",
+        hint: "Noe man kan bruke på kjøkkenet."
+    },
+    {
+        word: "ghostbusters",
+        hint: "Filmtittel som handler om en speiell form for jakt."
+    },
+    {
+        word: "terrasse",
+        hint: "En uteplass."
+    },
+    {
+        word: "ostepop",
+        hint: "Noe godteri"
+    },
+];
